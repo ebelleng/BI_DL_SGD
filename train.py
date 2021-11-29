760e428e4d4cfc23c092f32e6970048f58e88190
ebelleng <etienne.bellenger@gmail.com>
2021-11-29 07:52:13 -0300

    definitivo v2

--- train.py
+++ train.py
@@ -6,50 +6,34 @@ import my_utility as ut
 	
 # Softmax's training
 def train_softmax(x,y,param):
-    W     = ut.iniW(y.shape[0],x.shape[0])
-    V     = np.zeros(W.shape)
-    beta  = 0.9 
-    mu = param[1]
+    w = ut.iniW(y.shape[0],x.shape[0])
+    v = np.zeros(np.shape(w))
     costo = []
-    for iter in range(param[0]):        
-        gW, c = ut.grad_softmax(x,y,W,lambW=param[2])
-        
-        costo.append(c)
-
-        V = beta * V + mu * gW
-        W = W - V
-    return(W,costo)
+    for iter in range(param[0]):
+        gW,cost = ut.gradW_softmax(x,y,w) 
+        costo.append(cost)
+        w,v     = ut.updW_sft_sgd(w,v,gW,mu=param[1]) 
+    return(w,costo)
 
 # Training AE miniBatch SGD
 def train_batch(x,W,V,param):
     numBatch = np.int16(np.floor(x.shape[1]/param[0]))
     mu = param[2]
-    print(numBatch)
     for i in range(numBatch):
         #complete code  SGD with momentum  
         a = ut.forward_ae(x,W) 
-
-        # Calcular el error
-        error = np.sum( (a - x) ** 2) 
-        error = np.sum(error) * (1/2)
-        
-        # Calcular el gradiente oculto y salida    
-        dCdW, _ = ut.gradW_ae(a, x, W, error)
-        # Actualizar los pesos
-        W, V = ut.updW_ae_sgd(W, V, dCdW, mu)
+        gW, c = ut.gradW_ae(a,W)
+        W,V = ut.updW_ae_sgd(W,V,gW,mu)
     
     return (W,V)
 
-    return(W,V)
-
 #Training AE by use SGD
-def train_ae(x,hn,param):
-    print(x.shape, hn)    
+def train_ae(x,hn,param):   
     W,V    = ut.iniWs(x.shape[0],hn)
     for Iter in range(1,param[1]):        
         xe  = x[:,np.random.permutation(x.shape[1])]        
         W,V = train_batch(xe,W,V,param)                
-    return W
+    return W[0]
 
 #SAE's Training 
 def train_sae(x,param):    
@@ -58,11 +42,12 @@ def train_sae(x,param):
     for hn in range(3,len(param)):
         print('AE={} Hnode={}'.format(hn-2,param[hn]))
         # Se entrena el encoder
-        w, _ = train_ae(data, param[hn], param)
+        w = train_ae(data, param[hn], param)
         # Se guarda el peso del encoder
         W.append(w)
         # Se calcula la nueva data
         data = ut.act_sigmoid( np.dot(w, data))
+
     return(W,data)
 
 # Beginning ...
